f736a3a3bc8ebfa2d84d36b9e81c4944709a1c20
skyclouds2001 <skyclouds2001@163.com>
2024-10-17 04:41:07 +0800

    feat: add api idl file & update type definition file

--- index.d.ts
+++ index.d.ts
@@ -6,13 +6,6 @@ interface WorkerNavigator {
   readonly storageBuckets: StorageBucketManager
 }
 
-// [SecureContext]
-// interface mixin NavigatorStorageBuckets {
-//   [SameObject] readonly attribute StorageBucketManager storageBuckets;
-// };
-// Navigator includes NavigatorStorageBuckets;
-// WorkerNavigator includes NavigatorStorageBuckets;
-
 interface StorageBucketManager {
   open(name: string, options?: StorageBucketOptions): Promise<StorageBucket>
   keys(): Promise<string[]>
@@ -24,20 +17,6 @@ interface StorageBucketOptions {
   quota?: number
   expires?: DOMHighResTimeStamp
 }
-
-// [Exposed=(Window,Worker),
-//  SecureContext]
-// interface StorageBucketManager {
-//     Promise<StorageBucket> open(DOMString name, optional StorageBucketOptions options = {});
-//     Promise<sequence<DOMString>> keys();
-//     Promise<undefined> delete(DOMString name);
-// };
-
-// dictionary StorageBucketOptions {
-//   boolean persisted = false;
-//   unsigned long long quota;
-//   DOMHighResTimeStamp expires;
-// };
 
 interface StorageBucket {
   readonly name: string
@@ -56,23 +35,3 @@ interface StorageBucket {
 
   getDirectory(): Promise<FileSystemDirectoryHandle>
 }
-
-// [Exposed=(Window,Worker),
-//  SecureContext]
-// interface StorageBucket {
-//   readonly attribute DOMString name;
-
-//   [Exposed=Window] Promise<boolean> persist();
-//   Promise<boolean> persisted();
-
-//   Promise<StorageEstimate> estimate();
-
-//   Promise<undefined> setExpires(DOMHighResTimeStamp expires);
-//   Promise<DOMHighResTimeStamp?> expires();
-
-//   [SameObject] readonly attribute IDBFactory indexedDB;
-
-//   [SameObject] readonly attribute CacheStorage caches;
-
-//   Promise<FileSystemDirectoryHandle> getDirectory();
-// };
