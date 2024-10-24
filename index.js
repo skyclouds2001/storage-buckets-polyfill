c2c49ed994736ec44a3cf774dca42f725fbe7975
skyclouds2001 <skyclouds2001@163.com>
2024-10-24 20:29:32 +0800

    feat: meta data update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
     return
   }
 
-  const isBuiltinSupported = typeof StorageBucketManager !== 'undefined' && typeof StorageBucket !== 'undefined'
+  const isBuiltinSupported = typeof $StorageBucketManager !== 'undefined' && typeof $StorageBucket !== 'undefined'
 
   if (isBuiltinSupported) {
     return
@@ -25,52 +25,52 @@
 
   const MetaDataStorageKey = 'storage-buckets-polyfill'
 
-  const StorageBucketManager = function StorageBucketManager(sym) {
+  const $StorageBucketManager = function StorageBucketManager(sym) {
     if (!Object.is(sym, symbol)) {
       throw new TypeError('Illegal constructor')
     }
   }
 
-  Object.defineProperty(global, 'StorageBucketManager', {
-    configurable: true,
-    enumerable: false,
-    value: StorageBucketManager,
-    writable: true,
-  })
-
-  Object.defineProperty(StorageBucketManager.prototype, Symbol.toStringTag, {
+  Object.defineProperty($StorageBucketManager, 'name', {
     configurable: true,
     enumerable: false,
     value: 'StorageBucketManager',
     writable: false,
   })
 
+  Object.defineProperty($StorageBucketManager.prototype, Symbol.toStringTag, {
+    configurable: true,
+    enumerable: false,
+    value: 'StorageBucketManager',
+    writable: false,
+  })
+
+  Object.defineProperty(global, 'StorageBucketManager', {
+    configurable: true,
+    enumerable: false,
+    value: $StorageBucketManager,
+    writable: true,
+  })
+
   const $open = async function (name) {
     try {
-      const root = await global.navigator.storage.getDirectory()
-      await root.getFileHandle(MetaDataStorageKey)
+      const rootHandle = await global.navigator.storage.getDirectory()
+      await rootHandle.getFileHandle(MetaDataStorageKey)
     } catch (error) {
-      if (error instanceof DOMException && error.code === DOMException.NOT_SUPPORTED_ERR) {
-        const root = await global.navigator.storage.getDirectory()
-        const file = await root.getFileHandle(MetaDataStorageKey, { create: true })
-        const writable = await file.createWritable()
+      if (error instanceof DOMException && error.code === DOMException.NOT_FOUND_ERR) {
+        const rootHandle = await global.navigator.storage.getDirectory()
+        const fileHandle = await rootHandle.getFileHandle(MetaDataStorageKey, { create: true })
+        const writableStream = await fileHandle.createWritable()
         const data = JSON.stringify({})
-        await writable.write(data)
-        await writable.close()
+        await writableStream.write(data)
+        await writableStream.close()
       } else {
         throw error
       }
     }
-    const storageBucket = new StorageBucket(symbol, name)
+    const storageBucket = new $StorageBucket(symbol, name)
     return storageBucket
   }
-
-  Object.defineProperty(StorageBucketManager.prototype, 'open', {
-    configurable: true,
-    enumerable: false,
-    value: $open,
-    writable: false,
-  })
 
   Object.defineProperty($open, 'name', {
     configurable: true,
@@ -79,16 +79,24 @@
     writable: true,
   })
 
+  Object.defineProperty($StorageBucketManager.prototype, 'open', {
+    configurable: true,
+    enumerable: false,
+    value: $open,
+    writable: false,
+  })
+
   const $keys = async function () {
     try {
-      const root = await global.navigator.storage.getDirectory()
-      const file = await root.getFileHandle(MetaDataStorageKey)
-      const data = await file.getFile()
-      const list = await data.text()
-      const keys = Object.keys(JSON.parse(list))
+      const rootHandle = await global.navigator.storage.getDirectory()
+      const fileHandle = await rootHandle.getFileHandle(MetaDataStorageKey)
+      const file = await fileHandle.getFile()
+      const data = await file.text()
+      const entries = JSON.parse(data)
+      const keys = Object.keys(entries)
       return keys
     } catch (error) {
-      if (error instanceof DOMException && error.code === DOMException.NOT_SUPPORTED_ERR) {
+      if (error instanceof DOMException && error.code === DOMException.NOT_FOUND_ERR) {
         return []
       } else {
         throw error
@@ -103,7 +111,7 @@
     writable: false,
   })
 
-  Object.defineProperty(StorageBucketManager.prototype, 'keys', {
+  Object.defineProperty($StorageBucketManager.prototype, 'keys', {
     configurable: true,
     enumerable: true,
     value: $keys,
@@ -112,11 +120,11 @@
 
   const $delete = async function (name) {
     try {
-      const root = await global.navigator.storage.getDirectory()
-      const file = await root.getFileHandle(MetaDataStorageKey)
-      const data = await file.getFile()
-      const list = await data.text()
-      const entries = JSON.parse(list)
+      const rootHandle = await global.navigator.storage.getDirectory()
+      const fileHandle = await rootHandle.getFileHandle(MetaDataStorageKey)
+      const file = await fileHandle.getFile()
+      const data = await file.text()
+      const entries = JSON.parse(data)
       const entry = entries[name]
       if (entry == null) {
         return
@@ -130,10 +138,10 @@
         global.caches.delete(MetaDataStorageKey + name + el)
       })
       opfs.forEach((el) => {
-        root.removeEntry(MetaDataStorageKey + name + el)
+        rootHandle.removeEntry(MetaDataStorageKey + name + el)
       })
     } catch (error) {
-      if (error instanceof DOMException && error.code === DOMException.NOT_SUPPORTED_ERR) {
+      if (error instanceof DOMException && error.code === DOMException.NOT_FOUND_ERR) {
         return
       } else {
         throw error
@@ -148,14 +156,14 @@
     writable: false,
   })
 
-  Object.defineProperty(StorageBucketManager.prototype, 'delete', {
+  Object.defineProperty($StorageBucketManager.prototype, 'delete', {
     configurable: true,
     enumerable: true,
     value: $delete,
     writable: true,
   })
 
-  const StorageBucket = function StorageBucket(sym, name) {
+  const $StorageBucket = function StorageBucket(sym, name) {
     if (!Object.is(sym, symbol)) {
       throw new TypeError('Illegal constructor')
     }
@@ -168,21 +176,28 @@
     })
   }
 
-  Object.defineProperty(global, 'StorageBucket', {
-    configurable: true,
-    enumerable: false,
-    value: StorageBucket,
-    writable: true,
-  })
-
-  Object.defineProperty(StorageBucket.prototype, Symbol.toStringTag, {
+  Object.defineProperty($StorageBucket, 'name', {
     configurable: true,
     enumerable: false,
     value: 'StorageBucket',
     writable: false,
   })
 
-  const storageBuckets = new StorageBucketManager()
+  Object.defineProperty($StorageBucket.prototype, Symbol.toStringTag, {
+    configurable: true,
+    enumerable: false,
+    value: 'StorageBucket',
+    writable: false,
+  })
+
+  Object.defineProperty(global, 'StorageBucket', {
+    configurable: true,
+    enumerable: false,
+    value: $StorageBucket,
+    writable: true,
+  })
+
+  const storageBuckets = new $StorageBucketManager(symbol)
 
   Object.defineProperty(Object.getPrototypeOf(global.navigator), 'storageBuckets', {
     configurable: true,
